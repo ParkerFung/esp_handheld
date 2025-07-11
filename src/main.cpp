4b21db8b9fdd18905c4f545c44d452963dde81d0
parker <psalvadorf@gmail.com>
2025-07-11 14:54:27 -0500

    updated build config

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,32 @@
-#include <Arduino.h>
+#include <TFT_eSPI.h>  // Include the graphics library
+#include <SPI.h>
+#include "main.h"
 
-// put function declarations here:
-int myFunction(int, int);
-
+TFT_eSPI tft = TFT_eSPI();  // Create TFT instance
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  tft.init();               // Initialize the display
+  tft.setRotation(3);       // Rotate display: 0-3 depending on orientation
+
+  drawMenu();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Nothing here for now
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+
+void drawMenu(){
+
+  tft.fillScreen(TFT_SKYBLUE);
+  tft.setTextColor(TFT_BLACK);
+  tft.setTextSize(3);
+  tft.setTextFont(1);
+  tft.println("Main Menu\n\n");
+
+  tft.setTextSize(1);
+
+  tft.println("My Games\n");
+  tft.println("Settings\n");
+  tft.println("About");
+
 }
