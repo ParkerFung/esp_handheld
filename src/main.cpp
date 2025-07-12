bb77854ba18d28e9aaace619878e7db1641dec0b
parker <psalvadorf@gmail.com>
2025-07-12 01:20:02 -0500

    added main menu

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,96 @@
 #include <SPI.h>
 #include "main.h"
 
+const char* menuItems[] = {"My Games", "Settings", "About!!"};
+int selectedItem = 0;
+bool lastButtonState = HIGH;
+bool lastSelectState = HIGH;
+#define BUTTON_DOWN 21
+#define BUTTON_SELECT 15
+
 TFT_eSPI tft = TFT_eSPI();  // Create TFT instance
+
 void setup() {
-  tft.init();               // Initialize the display
-  tft.setRotation(3);       // Rotate display: 0-3 depending on orientation
+  tft.init();
+  tft.setRotation(3);
+  pinMode(BUTTON_DOWN, INPUT_PULLUP);
+  pinMode(BUTTON_SELECT, INPUT_PULLUP);  // <== Add this
+  drawMenu();
+}
+
+
+void loop() {
+  downButton();
+  selectButton();  // <== handle SELECT press
+}
+
+
+void drawMenu() {
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.setTextSize(3);
+  tft.setCursor(20, 10);
+  tft.println("Main Menu");
+
+  // Draw menu items at fixed Y positions
+  for (int i = 0; i < 3; i++) {
+    int y = 50 + i * 30;
+    
+    // Highlight or draw arrow
+    if (i == selectedItem) {
+      tft.setCursor(10, y);
+      tft.print(">");
+    } else {
+      tft.setCursor(10, y);
+      tft.print(" ");  // Erase any previous arrow
+    }
+
+    // Draw menu label
+    tft.setCursor(30, y);
+    tft.print(menuItems[i]);
+  }
+}
+
+void aboutPage(){
+  tft.fillScreen(TFT_BLACK);
+  tft.setCursor(10, 30);
+  tft.setTextColor(TFT_WHITE);
+  tft.setTextSize(2);
+  tft.println("This device was created");
+  tft.println(" with an ESP32");
+  tft.println("by Parker and Weston Fung!!");
+
+  delay(5000);
 
   drawMenu();
 }
 
-void loop() {
-  // Nothing here for now
+void menuSelect(int selectedItem){
+  switch(selectedItem){
+    case 2:
+      aboutPage();
+      break;
+    default: return;
+
+  }
 }
 
+void downButton(){
+bool currentButtonState = digitalRead(BUTTON_DOWN);
+if (currentButtonState == LOW && lastButtonState == HIGH) {
+  selectedItem = (selectedItem + 1) % 3;
+  drawMenu();
+  delay(200);  // debounce
+}
+lastButtonState = currentButtonState;
+}
 
-void drawMenu(){
-
-  tft.fillScreen(TFT_SKYBLUE);
-  tft.setTextColor(TFT_BLACK);
-  tft.setTextSize(3);
-  tft.setTextFont(1);
-  tft.println("Main Menu\n\n");
-
-  tft.setTextSize(1);
-
-  tft.println("My Games\n");
-  tft.println("Settings\n");
-  tft.println("About");
-
+void selectButton() {
+  bool currentSelectState = digitalRead(BUTTON_SELECT);
+  if (currentSelectState == LOW && lastSelectState == HIGH) {
+    // Button SELECT just pressed
+    menuSelect(selectedItem);
+    delay(200);  // debounce
+  }
+  lastSelectState = currentSelectState;
 }
--- src/main.h
+++ src/main.h
@@ -1,1 +1,5 @@
 void drawMenu();
+void downButton();
+void menuSelect(int selectedItem);
+void aboutPage();
+void selectButton();
