0463a69a346a9d82921ffa6ef2b053fbae91a7ad
parker <psalvadorf@gmail.com>
2025-07-12 14:42:51 -0500

    added up button input

--- include/main.h
+++ include/main.h
@@ -9,16 +9,19 @@ extern const char* menuItems[];
 extern int selectedItem;
 extern bool lastButtonState;
 extern bool lastSelectState;
+extern bool lastUpState; 
 
 // Button pins
 #define BUTTON_DOWN 21
 #define BUTTON_SELECT 15
+#define BUTTON_UP 10
 
 // Function declarations
 void drawMenu();
 void aboutPage();
 void menuSelect(int selectedItem);
 void downButton();
+void upButton();
 void selectButton();
 
 #endif
--- src/input.cpp
+++ src/input.cpp
@@ -10,6 +10,22 @@ void downButton() {
   lastButtonState = currentButtonState;
 }
 
+void upButton(){
+  bool currentUpState = digitalRead(BUTTON_UP);
+  if(currentUpState == LOW && lastUpState == HIGH){
+    if(selectedItem <= 0){
+      selectedItem == 2;
+    }
+    else{
+      selectedItem = (selectedItem - 1) % 3;
+    }
+    drawMenu();
+    delay(200);
+  }
+  lastUpState = currentUpState;
+
+}
+
 void selectButton() {
   bool currentSelectState = digitalRead(BUTTON_SELECT);
   if (currentSelectState == LOW && lastSelectState == HIGH) {
