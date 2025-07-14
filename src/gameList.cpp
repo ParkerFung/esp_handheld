ee2cb5440912da5921589931127a0fcb2bda49de
parker <psalvadorf@gmail.com>
2025-07-14 12:02:57 -0500

    sync

---
+++ src/gameList.cpp
@@ -0,0 +1,14 @@
+#include "main.h"
+
+const char* games[] = {"Pong", "Breakout", "Snake", "Tile"};
+
+void DrawGameList(){
+
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextColor(TFT_WHITE);
+
+
+
+
+
+}
